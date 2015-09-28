53401d30a397b17ad84b42006074267604ade898
naterish <naterish@yandex.ru>
2015-09-28 11:54:45 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
+echo "Install nginx \n"
+apt-get install nginx -y
+echo "Nginx installed \n"
+echo "Move nginx config"
+mv ./nginx/y-translate.conf /etc/nginx/sites-enabled/y-translate.conf
+mv ./nginx/g-translate.conf /etc/nginx/sites-enabled/g-translate.conf
+nginx -t
+service nginx restart
+echo "Nginx configurated"
+echo "Install php5 fpm"
+apt-get install php5-fpm -y
+echo "Install php5 fpm installed"
+mkdir -p /var/www/php/g-translate
+mkdir -p /var/www/php/y-translate
+chmod 777 /var/www/php
+mv ./g-translate/index.php /var/www/php/g-translate/index.php
+mv ./y-translate/index.php /var/www/php/y-translate/index.php
 
